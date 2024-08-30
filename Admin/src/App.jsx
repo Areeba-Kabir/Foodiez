0d4da2dd27ba42389a78e10af19f6ede6801015d
Areeeba <areeebakabir137@gmail.com>
2024-08-30 19:35:52 +0500

    19th commit | Frontend | Admin Panel | setted up sidebar links

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/navabr/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Additem from "./pages/AddItem/Additem";
+import Orders from "./pages/Orders/Orders";
+import List from "./pages/List/List";
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Additem />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/list" element={<List />} />
+        </Routes>
       </div>
     </div>
   );
--- Admin/src/components/sidebar/Sidebar.jsx
+++ Admin/src/components/sidebar/Sidebar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { assets } from "../../assets/assets";
 import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-options">
-        <div className="sidebar-option">
+        <NavLink to='/add' className="sidebar-option">
           <img src={assets.add_icon_white} alt="add_icon_white" />
           <p>Add Item</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to='/list' className="sidebar-option">
           <img src={assets.parcel_icon} alt="parcel_icon" />
           <p>Item List</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to='/orders' className="sidebar-option">
           <img className="orders" src={assets.order_icon} alt="order_icon" />
           <p>Orders</p>
-        </div>
+        </NavLink>
       </div>
     </div>
   );
