9a156c286e4e4a6c25a0c9d7a826669e50263a6e
Areeeba <areeebakabir137@gmail.com>
2024-09-03 23:30:51 +0500

    36th commit | backend | middleware

--- Foodie_backend/middlewares/auth.js
+++ Foodie_backend/middlewares/auth.js
@@ -1,5 +1,19 @@
 import jwt from "jsonwebtoken";
 
-const authMiddleware = async (req, res, next) => {};
+const authMiddleware = async (req, res, next) => {
+  const { token } = req.headers;
+  if (!token) {
+    return res.json({ success: false, message: "not Authorized Login again" });
+  }
+
+  try {
+    const token_decode = jwt.verify(token, process.env.jwt_secret);
+    req.body.userId = token_decode.id;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 export default authMiddleware;
